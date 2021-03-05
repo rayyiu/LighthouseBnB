41f104441a88105ee3695a8f4eb3bd174a7f4eec
Ray Yiu <rayyiu66@gmail.com>
2021-03-04 16:11:55 -0800

    added functionality for getallreservations to use psql

--- server/database.js
+++ server/database.js
@@ -67,7 +67,12 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT properties.*, reservations.start_date, reservations.end_date FROM 
+  reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE guest_id = $1
+  LIMIT $2;`, [guest_id, limit])
+    .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
